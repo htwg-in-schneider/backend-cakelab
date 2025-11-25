0157c53ae0d8d9c422fce39a0628a849b83668cc
Anina <Anina.Spaeth@htwg-konstanz.de>
2025-11-25 08:42:01 +0100

    vorbereitung iterationen

---
+++ src/test/cakelab/backend/CakelabBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cakelab.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakelabBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+    
+}
