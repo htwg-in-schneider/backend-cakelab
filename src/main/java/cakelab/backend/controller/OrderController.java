1763677eaf2fff4f8c07a06be358da272d4061bb
al871boy <ale871boy@htwg-konstanz.de>
2026-01-11 20:33:54 +0100

    CrossOrign

--- src/main/java/cakelab/backend/controller/OrderController.java
+++ src/main/java/cakelab/backend/controller/OrderController.java
@@ -10,6 +10,7 @@ import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/orders")
 public class OrderController {
 
