9f2076e4b9148cfc6c5dddb8f7c989081603be72
al871boy <ale871boy@htwg-konstanz.de>
2026-01-11 20:25:54 +0100

    verbesserung

--- src/main/java/cakelab/backend/config/SecurityConfig.java
+++ src/main/java/cakelab/backend/config/SecurityConfig.java
@@ -1,28 +1,27 @@
 package cakelab.backend.config;
 
 import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
-
+/**
+ * Configures our application with Spring Security to restrict access to our API
+ * endpoints.
+ */
 @Configuration
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-                .csrf(csrf -> csrf.disable()) 
+                .cors(withDefaults())
                 .authorizeHttpRequests((authorize) -> authorize
                         .requestMatchers("/api/profile").authenticated()
-                        .requestMatchers("/api/orders", "/api/orders/**").authenticated()
+                        .requestMatchers("/api/orders").authenticated()
                         .requestMatchers(HttpMethod.POST, "/api/cake", "/api/cake/*").authenticated()
                         .requestMatchers(HttpMethod.PUT, "/api/cake/*").authenticated()
                         .requestMatchers(HttpMethod.DELETE, "/api/cake/*").authenticated()
@@ -31,17 +30,5 @@ public class SecurityConfig {
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(withDefaults()))
                 .build();
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173")); 
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Cache-Control"));
-        configuration.setAllowCredentials(true);       
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
     }
 }
